96ae9d6c23e79c27179ef2ac9fcd29a07b9d612d
Jose Soto <jsoto7087@gmail.com>
2024-05-05 21:04:51 -0400

    adds tailwind css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ function App() {
 
 
     return (
-        <>
+        <div>
           <main>
             <Header />
             <Routes>
@@ -15,7 +15,7 @@ function App() {
               <Route path="/" element={ <BirdPage /> }/>
             </Routes>
           </main>
-        </>
+        </div>
       );
 }
 
