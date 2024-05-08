fc78371a8002d8f9b7361647f4a8e60db51cf2a8
Jose Soto <jsoto7087@gmail.com>
2024-05-07 22:21:38 -0400

    fix deployed error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
-import BirdPage from "./BirdPage";
+// import BirdPage from "./BirdPage";
 import BirdDetail from "./BirdDetail"
 import Hero from "./Hero";
 
