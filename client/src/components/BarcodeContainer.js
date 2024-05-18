9d1cbed3c7bc638abd5a5bf5bf090d7b4484ac6e
Jose Soto <jsoto7087@gmail.com>
2024-05-17 20:22:09 -0400

    test commit

---
+++ client/src/components/BarcodeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BarcodeReader from "./barcode/BarcodeReader";
+
+
+export default function BarcodeContainer() {
+
+  return (
+    <div>
+      <BarcodeReader />
+    </div>
+  )
+}
