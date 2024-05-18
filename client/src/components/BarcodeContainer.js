915d410eb8783a706988fa9e78d6dc53ad133fae
Jose Soto <jsoto7087@gmail.com>
2024-05-18 00:22:49 -0400

    adds data context

--- client/src/components/BarcodeContainer.js
+++ client/src/components/BarcodeContainer.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Html5QrcodePlugin from "./barcode/Html5QrcodePlugin";
-
+import { DataContext } from "../context/DataContextProvider";
 
 
 export default function BarcodeContainer() {
   
+  const { carData, setCarData } = useContext(DataContext)
+
+
   const onNewScanResult = (decodedText, decodedResult, setDecodedResults) => {
     console.log("App [result]", decodedResult);
     console.log("DECODED:", decodedText);
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import { DataContextProvider } from "./context/DataContextProvider";
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <DataContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DataContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
