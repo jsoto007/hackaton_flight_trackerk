834d20aa388a8c2460f8d013611bab36a84f6152
Jose Soto <jsoto7087@gmail.com>
2024-05-25 00:50:47 -0400

    adds location

--- client/src/components/HomeContainer.js
+++ client/src/components/HomeContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import CarList from "./CarList";
-
+import UserLocation from "./location/UserLocation";
 
 export default function HomeContainer() {
 
   return(
     <div>
       <CarList />
+      <UserLocation />
     </div>
   )
 }
--- client/src/components/barcode/VinDecoder.js
+++ client/src/components/barcode/VinDecoder.js
@@ -5,13 +5,13 @@ import vinDecoder from 'vin-decode';
 
 export default function VinDecoder( { onVinData } ) {
 
-  const [vinNumber, setVinNumber] = useState([])
+  const [decodedVin, setDeodedVin] = useState([])
 
   useEffect(() => {
     if (onVinData) {
       const decodedVin = vinDecoder(`${onVinData.decodedText}`).decode();
 
-      return setVinNumber(decodedVin) 
+      return setDeodedVin(decodedVin) 
     }
 
   }, [onVinData])
@@ -20,9 +20,9 @@ export default function VinDecoder( { onVinData } ) {
     <div className="mb-6 bg-neutral-200 rounded-sm">
       <p>
         <em className="font-bold"> Year:</em>
-        <em className="bg-green-100 rounded-md px-1 mx-2">{vinNumber.year}</em>
+        <em className="bg-green-100 rounded-md px-1 mx-2">{decodedVin.year}</em>
         <em className="font-bold">Manufacturer:</em>
-        <em className="bg-green-100 rounded-md px-1 mx-2">{vinNumber.manufacturer}</em>
+        <em className="bg-green-100 rounded-md px-1 mx-2">{decodedVin.manufacturer}</em>
         </p>
     </div>
   )
